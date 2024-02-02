b27656a7222c24cc21e56909420c8dfa05b6829f
Nikunj Sutariya <nikunjs@zignuts.com-at-162555042902>
2024-02-02 10:58:45 +0530

    first commit

---
+++ lib/homepage/input_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class InputPage extends StatelessWidget {
+  const InputPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
